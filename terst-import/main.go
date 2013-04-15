21269755eef75179d4234bb30531b27612c30599
Robert Krimen <robertkrimen@gmail.com>
2013-04-14 19:02:45 -0700

    Add terst-import

---
+++ terst-import/main.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+	"github.com/robertkrimen/smuggol"
+	_ "github.com/robertkrimen/terst"
+)
+
+func main() {
+	smuggol.Main("terst-import", "github.com/robertkrimen/terst", nil)
+}
